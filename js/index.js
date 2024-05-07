bbf884db66ba3e1fc48a41181a4dd2a408cac033
Oguzhan Dogan <oguzhandogan@Ozzys-MacBook-Pro.local>
2024-05-07 01:24:43 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -12,13 +12,61 @@ footer.appendChild(copyright);
 const skills = ["JavaScript", "HTML", "CSS", "Adobe Photoshop", "GitHub"];
 const skillsSection = document.querySelector('#skills');
 const skillsList = skillsSection.querySelector('ul');
-skillsList.style.display = 'table';
-skillsList.style.backgroundColor = 'hsla(60, 46%, 73%, 0.8)';
-skillsList.style.padding = '10px 30px';
-skillsList.style.borderRadius = '5px';
 skills.forEach(skill => {
     const eachSkill = document.createElement('li');
     eachSkill.innerText = skill;
     skillsList.appendChild(eachSkill);
 });
+ 
+//Messages
+const leaveMessage = document.forms['leave_message'];
+leaveMessage.addEventListener('submit', (event) => {
+    event.preventDefault();
 
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+
+    console.log(`Name: ${usersName}, Email: ${usersEmail}, Message: ${usersMessage}`);
+
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.querySelector('ul');
+
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = 
+    `<a href="mailto:${usersEmail}">${usersName}</a> 
+    <span>${usersMessage}</span>`;
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'Remove';
+    removeButton.type = 'button';
+    removeButton.style.marginLeft = "10px";
+
+    removeButton.addEventListener('click', () => {
+        const entry = removeButton.parentNode;
+        entry.remove();
+        if(messageList.children.length === 0){
+            messageSection.hidden = true;
+        }
+    });
+    messageSection.hidden = false;
+
+    const editButton = document.createElement('button');
+    editButton.innerText = 'Edit';
+    editButton.type = 'button';
+    editButton.style.marginLeft = "10px";
+
+    editButton.addEventListener('click', () => {
+        const newMessageText = prompt('Edit your message:', usersMessage);
+        if (newMessageText !== null) {
+            newMessage.querySelector('span').innerText = newMessageText;
+        }
+    });
+
+    newMessage.appendChild(removeButton);
+    newMessage.appendChild(editButton);
+    messageList.appendChild(newMessage);
+    
+    
+    leaveMessage.reset();
+}); 
